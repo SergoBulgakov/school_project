52071499a91306c54e85cb39e9ceedc34533d4f8
SergoBulgakov <serg.navernoe88@gmail.com>
2024-05-14 03:50:29 +0300

    add defense events, edit profile logic (not finished yet)

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Project, Project_defense, Content_point
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Project_defense)
+admin.site.register(Content_point)
--- home/models.py
+++ home/models.py
@@ -8,9 +8,17 @@ import uuid
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+
+    name = models.CharField(max_length=30, null=True)
+    surname = models.CharField(max_length=30, null=True)
+    patronymic = models.CharField(max_length=30, null=True)
+    school_class = models.CharField(max_length=10, null=True)
+    email = models.EmailField(null=True)
+    olympiad = models.BooleanField(null=True)
+    achievment = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.user.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -31,7 +39,7 @@ class Project(models.Model):
     teacher = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name="Teacher") #связь с научным руководителем
 
     theme = models.CharField(max_length=200) #тема проекта
-    subject = models.TextField('Subject',max_length=100) #предмет
+    subject = models.CharField(max_length=100) #предмет
     link_project = models.CharField(max_length=200)
     link_annex = models.CharField(max_length=200)
     link_review = models.CharField(max_length=200)
@@ -40,10 +48,34 @@ class Project(models.Model):
 
 
     def __str__(self):
-        """
-        String for representing the Model object.
-        """
         return self.theme
+    
+
+class Project_defense(models.Model):
+    
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4) #id 
+
+    project = models.ForeignKey(Project,on_delete=models.SET_NULL,null=True,related_name="Project_project_defense") #связь с проектом
+    exspert1 = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name="Expert1_project_defense") 
+    exspert2 = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name="Expert2_project_defense") 
+    exspert3 = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name="Expert3_project_defense") #связь с 3 экспертами
+
+    date = models.DateField() #дата защиты
+    
+    measure1exspert1 = models.SmallIntegerField # оценка 1 критерия 1 экспертом
+    measure1exspert2 = models.SmallIntegerField # оценка 1 критерия 2 экспертом
+    measure1exspert3 = models.SmallIntegerField # оценка 1 критерия 3 экспертом
+    measure2exspert1 = models.SmallIntegerField # оценка 2 критерия 1 экспертом
+    measure2exspert2 = models.SmallIntegerField # оценка 2 критерия 2 экспертом
+    measure2exspert3 = models.SmallIntegerField # оценка 2 критерия 3 экспертом
+    measure3exspert1 = models.SmallIntegerField # оценка 3 критерия 1 экспертом
+    measure3exspert2 = models.SmallIntegerField # оценка 3 критерия 2 экспертом
+    measure3exspert3 = models.SmallIntegerField # оценка 3 критерия 3 экспертом
+    link_call = models.CharField(max_length=200) # ссылка на конференцию
+
+
+    def __str__(self):
+        return self.project.theme
 
 
     # def get_absolute_url(self):
@@ -51,3 +83,28 @@ class Project(models.Model):
     #     Returns the url to access a particular book instance.
     #     """
     #     return reverse('book-detail', args=[str(self.id)])
+
+class Content_point(models.Model):
+    
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4) #id проекта
+
+    project = models.ForeignKey(Project,on_delete=models.SET_NULL,null=True,related_name="Project_content_points") #связь с проектом
+    exspert = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name="Expert_content_points") #связь с экспертом
+
+    date = models.DateField() #дата оценивания
+
+    measure1 = models.SmallIntegerField
+    measure2 = models.SmallIntegerField
+    measure3 = models.SmallIntegerField
+    measure4 = models.SmallIntegerField
+    measure5 = models.SmallIntegerField
+    measure6 = models.SmallIntegerField
+    measure7 = models.SmallIntegerField
+    measure8 = models.SmallIntegerField
+    measure9 = models.SmallIntegerField #оценки по 9 критериям
+
+    link_anti_plagiarism = models.CharField(max_length=200) # ссылка на антиплагиат
+
+
+    def __str__(self):
+        return self.project.theme
